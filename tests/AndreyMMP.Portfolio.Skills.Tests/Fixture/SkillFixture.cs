ec1fd3719d9db2498f21285411b7264243826d98
Andrey Malaquias Medeiros Pinto <andrey.mmp@outlook.com>
2024-03-12 16:18:22 -0400

    Inclusão de testes unitarios para o método CreateSkill da camada Application

---
+++ tests/AndreyMMP.Portfolio.Skills.Tests/Fixture/SkillFixture.cs
@@ -0,0 +1,14 @@
+﻿namespace AndreyMMP.Portfolio.Skills.Tests.Fixture
+{
+    public static class SkillFixture
+    {
+        private static Random random = new Random();
+
+        public static string CreateString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ tests/AndreyMMP.Portfolio.Skills.Tests/ServicesTests/SkillServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using AndreyMMP.Portfolio.Skills.Application.DTO;
+using AndreyMMP.Portfolio.Skills.Application.Response;
+using AndreyMMP.Portfolio.Skills.Application.Services;
+using AndreyMMP.Portfolio.Skills.Domain.Interfaces;
+using AndreyMMP.Portfolio.Skills.Domain.Models;
+using AndreyMMP.Portfolio.Skills.Tests.Fixture;
+using AutoMapper;
+using Moq;
+
+namespace AndreyMMP.Portfolio.Skills.Tests.ServicesTests
+{
+    public class SkillServiceTests
+    {
+        private readonly Mock<ISkillRepository> _skillRepository;
+        private readonly Mock<IMapper> _mapper;
+        private readonly SkillService _skillService;
+
+        public SkillServiceTests()
+        {
+            _skillRepository = new Mock<ISkillRepository>();
+            _mapper = new Mock<IMapper>();
+            _skillService = new SkillService(_skillRepository.Object, _mapper.Object);
+        }
+
+        [Fact]
+        public async Task CreateSkill_WithEmptyName_ShouldThrowExeception()
+        {
+            var result = () => _skillService.CreateSkill(new SkillDTO { Name = string.Empty });
+
+            Exception ex = await Assert.ThrowsAsync<ArgumentException>(result);
+            Assert.Equal(SkillResponse.SkillNameCantBeEmpty, ex.Message);
+        }
+
+        [Fact]
+        public async Task CreateSkill_WithName_ShouldNotThrowExeception()
+        {
+            var skillDTO = new SkillDTO { Name = SkillFixture.CreateString(10) };
+            _skillRepository.Setup(s => s.CreateSkill(new Skill { Name = skillDTO.Name })).Returns(Task.CompletedTask);
+            
+            var result = _skillService.CreateSkill(skillDTO);
+            Assert.True(result.Exception == null);
+        }
+    }
+}
