2a45f22baf835230570853688a3874d0958b79f7
Andrey Malaquias Medeiros Pinto <andrey.mmp@outlook.com>
2024-03-19 15:58:12 -0400

    Inclusão de testes unitários para método DeleteSkill da camada API

--- tests/AndreyMMP.Portfolio.Skills.Tests/ControllersTests/SkillControllerTests.cs
+++ tests/AndreyMMP.Portfolio.Skills.Tests/ControllersTests/SkillControllerTests.cs
@@ -41,7 +41,7 @@ namespace AndreyMMP.Portfolio.Skills.Tests.ControllersTests
         }
 
         [Fact]
-        public async Task CreateSkill_WithName_ShouldReturnSuccessMessage()
+        public async Task CreateSkill_WithName_ShouldReturnSkillCreatedMessage()
         {
             var skillDTO = new SkillDTO { Name = SkillFixture.CreateString(10) };
             _skillRepository.Setup(s => s.CreateSkill(It.IsAny<Skill>())).Returns(Task.CompletedTask);
@@ -57,7 +57,7 @@ namespace AndreyMMP.Portfolio.Skills.Tests.ControllersTests
         }
 
         [Fact]
-        public async Task UpdateSkill_WithEmptyId_ShouldReturnFailureMessage()
+        public async Task UpdateSkill_WithEmptyId_ShouldReturnNoSkillRecordsFoundMessage()
         {
             var skillDTO = new SkillDTO() { Name = SkillFixture.CreateString(10) };            
 
@@ -72,7 +72,7 @@ namespace AndreyMMP.Portfolio.Skills.Tests.ControllersTests
         }
 
         [Fact]
-        public async Task UpdateSkill_WithIdButNoSkillsRecords_ShouldReturnFailureMessage()
+        public async Task UpdateSkill_WithIdButNoSkillsRecords_ShouldReturnNoSkillRecordsFoundMessage()
         {
             var skillDTO = new SkillDTO() { Id = SkillFixture.CreateInt(), Name = SkillFixture.CreateString(10) };
             _skillRepository.Setup(s => s.GetSkillById(skillDTO.Id)).Returns(Task.FromResult((Skill)null));
@@ -88,7 +88,7 @@ namespace AndreyMMP.Portfolio.Skills.Tests.ControllersTests
         }
 
         [Fact]
-        public async Task UpdateSkill_WithEmptyName_ShouldReturnFailureMessage()
+        public async Task UpdateSkill_WithEmptyName_ShouldReturnSkillNameCantBeEmptyMessage()
         {
             var skillDTO = new SkillDTO() { Id = SkillFixture.CreateInt() };
             _skillRepository.Setup(s => s.GetSkillById(skillDTO.Id)).Returns(Task.FromResult(new Skill() { Id = skillDTO.Id }));
@@ -104,7 +104,7 @@ namespace AndreyMMP.Portfolio.Skills.Tests.ControllersTests
         }
 
         [Fact]
-        public async Task UpdateSkill_WithIdAndName_ShouldReturnSuccessMessage()
+        public async Task UpdateSkill_WithIdAndName_ShouldReturnSkillUpdatedMessage()
         {
             var skillDTO = new SkillDTO() { Id = SkillFixture.CreateInt(), Name = SkillFixture.CreateString(10) };
             _skillRepository.Setup(s => s.GetSkillById(skillDTO.Id)).Returns(Task.FromResult(new Skill { Id = skillDTO.Id, Name = SkillFixture.CreateString(10) }));
@@ -119,6 +119,39 @@ namespace AndreyMMP.Portfolio.Skills.Tests.ControllersTests
             Assert.True(result.Exception == null);
             _skillRepository.Verify(s => s.GetSkillById(It.IsAny<int>()), Times.Once);
             _skillRepository.Verify(s => s.UpdateSkill(It.IsAny<Skill>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task DeleteSkill_WithEmptyId_ShouldReturnNoSkillRecordsFoundMessage()
+        {
+            var skillDTO = new SkillDTO() { Name = SkillFixture.CreateString(10) };
+
+            var result = _skillController.DeleteSkill(skillDTO.Id);
+            await Task.Run(() => result);
+
+            var badRequestObjectResult = result.Result as BadRequestObjectResult;
+            Assert.Equal(SkillResponse.NoSkillRecordsFound, badRequestObjectResult.Value);
+            Assert.Equal(StatusCodes.Status400BadRequest, badRequestObjectResult.StatusCode);
+            _skillRepository.Verify(s => s.GetSkillById(It.IsAny<int>()), Times.Once);
+            _skillRepository.Verify(s => s.CreateSkill(It.IsAny<Skill>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task DeleteSkill_WithId_ShouldReturnSkillDeletedMessage()
+        {
+            var skillDTO = new SkillDTO() { Id = SkillFixture.CreateInt(), Name = SkillFixture.CreateString(10) };
+            _skillRepository.Setup(s => s.GetSkillById(skillDTO.Id)).Returns(Task.FromResult(new Skill { Id = skillDTO.Id, Name = SkillFixture.CreateString(10) }));
+            _skillRepository.Setup(s => s.DeleteSkill(It.IsAny<int>())).Returns(Task.CompletedTask);
+
+            var result = _skillController.DeleteSkill(skillDTO.Id);
+            await Task.Run(() => result);
+
+            var okObjectResult = result.Result as OkObjectResult;
+            Assert.Equal(SkillResponse.SkillDeleted, okObjectResult.Value);
+            Assert.Equal(StatusCodes.Status200OK, okObjectResult.StatusCode);
+            Assert.True(result.Exception == null);
+            _skillRepository.Verify(s => s.GetSkillById(It.IsAny<int>()), Times.Once);
+            _skillRepository.Verify(s => s.DeleteSkill(It.IsAny<int>()), Times.Once);
         }
     }    
 }
